ed393f05462e45265e3679b021b3e2a0e72af9df
Christian Franco <christian.franco94@icloud.com>
2017-12-04 01:30:40 -0800

    [WIP] Cleaning up mediaDB api & providers api

--- server/mediaDB/index.js
+++ server/mediaDB/index.js
@@ -3,55 +3,55 @@ const Provider = require("./providers/index").tmdb;
 const API_KEY = "26b55e337bd07a6a46810bce43adcaf4";
 
 class MediaDB {
-  constructor({ provider }) {
-    this.providers = [];
-    this.provider = provider;
+  constructor({ providers=[], defaultProvider='' }) {
+    if (!providers.length) { throw { message: 'No providers have been provided, failed to create instance.'}};
+    this.providers = providers;
+    this.provider = defaultProvider ? this.switchProvider(defaultProvider) : this.providers[0];
   }
-  switchProvider(provider) {
-    this.provider = provider;
+  switchProvider(name) {
+    return this.providers.reduce((selectedProvider, provider) => provider.name === name ? provider : selectedProvider);
+  }
+  async getPopular(type, options) {
+    const { provider } = this.provider;
+    return provider.getPopular(type, options);
   }
   async getPopularMovies() {
-    return await this.provider.getPopularMovies();
+    const { provider } = this.provider;
+    return provider.getPopularMovies();
   }
-  async getTopRated() {
-    return await this.provider.getTopRated();
+  async getTopRated(type, options) {
+    const { provider } = this.provider;
+    return provider.getTopRated(options);
   }
-  // async init() {
-  //   this.providers.map(async provider => {
-  //     return provider.init();
-  //   });
-  //   return Promise.all()
-  // }
+  async init() {
+    this.providers = await Promise.all(
+      this.providers.map(async ({name, provider}) =>  ({ name, provider: await provider.init()})
+    ));
+    return this;
+  }
 }
 
-const provider = new Provider({ API_KEY });
-const mediaDB = new MediaDB({ provider });
-
-module.exports = mediaDB;
-
-
-
-
+module.exports = MediaDB;
 
 (async () => {
   try {
-    const provider = await new Provider({API_KEY}).init();
+    const TMDBProvider = new Provider({API_KEY});
+    
+    const providers = [
+      { name: 'TMDB', provider: TMDBProvider },
+    ];
 
+    const mediaDB = await new MediaDB({ providers, defaultProvider: 'TMDB' }).init();
 
-
-    const mediaDb = await new MediaDB({providers: []}).init();
-
-
-
-
-
-
-
-    console.log(provider);
+    const movies = await mediaDB.getPopular('movies', {
+      page: 2
+    });
+    const movies2 = await mediaDB.getTopRated('movies', {
+      page: 1
+    });
+    // console.log(movies2);
   }
   catch (e) {
-    console.log(e);
+    console.log('Error Catcher: ', e);
   }
-
-    console.log('after');
 })();
--- server/mediaDB/providers/tmdb/index.js
+++ server/mediaDB/providers/tmdb/index.js
@@ -1,51 +1,148 @@
 const axios = require("axios");
-const generateAPI = (endpoint, API_KEY, page) => {
-    return `https://api.themoviedb.org/3/${endpoint}/?api_key=${API_KEY}&page=${page}`;
-  };
-  
-const popular = (API_KEY, page) => generateAPI("movie/popular", API_KEY, page);
-const topRated = (API_KEY, page) => generateAPI('movie/top_rated', API_KEY, page);
 
 class TMDB {
-    constructor({API_KEY}) {
+    constructor({ API_KEY }) {
         this.API_KEY = API_KEY;
-        this.buckets = {
-            popular: []
+        this.API_URL = 'https://api.themoviedb.org/3/';
+        this.config = {
+            "images": {
+              "base_url": "http://image.tmdb.org/t/p/",
+              "secure_base_url": "https://image.tmdb.org/t/p/",
+              "backdrop_sizes": [
+                "w300",
+                "w780",
+                "w1280",
+                "original"
+              ],
+              "logo_sizes": [
+                "w45",
+                "w92",
+                "w154",
+                "w185",
+                "w300",
+                "w500",
+                "original"
+              ],
+              "poster_sizes": [
+                "w92",
+                "w154",
+                "w185",
+                "w342",
+                "w500",
+                "w780",
+                "original"
+              ],
+              "profile_sizes": [
+                "w45",
+                "w185",
+                "h632",
+                "original"
+              ],
+              "still_sizes": [
+                "w92",
+                "w185",
+                "w300",
+                "original"
+              ]
+            },
+            "change_keys": [
+              "adult",
+              "air_date",
+              "also_known_as",
+              "alternative_titles",
+              "biography",
+              "birthday",
+              "budget",
+              "cast",
+              "certifications",
+              "character_names",
+              "created_by",
+              "crew",
+              "deathday",
+              "episode",
+              "episode_number",
+              "episode_run_time",
+              "freebase_id",
+              "freebase_mid",
+              "general",
+              "genres",
+              "guest_stars",
+              "homepage",
+              "images",
+              "imdb_id",
+              "languages",
+              "name",
+              "network",
+              "origin_country",
+              "original_name",
+              "original_title",
+              "overview",
+              "parts",
+              "place_of_birth",
+              "plot_keywords",
+              "production_code",
+              "production_companies",
+              "production_countries",
+              "releases",
+              "revenue",
+              "runtime",
+              "season",
+              "season_number",
+              "season_regular",
+              "spoken_languages",
+              "status",
+              "tagline",
+              "title",
+              "translations",
+              "tvdb_id",
+              "tvrage_id",
+              "type",
+              "video",
+              "videos"
+            ]
+        };
+    }
+    generateAPI(endpoint='', api_key, options=[]) {
+        if(!endpoint) { throw { message: 'GenerateAPI: No endpoint path provided for api generation'}};
+        if(!api_key) { throw { message: 'GenerateAPI: Requires an api key'}}
+        return options.reduce((apiUrl, option) => {
+            return `${apiUrl}&${option.key}=${option.value}`;
+        }, `${this.API_URL}${endpoint}?api_key=${api_key}`);
+    };
+
+    async getPopular(type, options = { page: 1, adult: false }) {
+        switch(type) {
+            case 'movies':
+                return this.getPopularMovies(options);
+            break;
+            default: 
+                throw { message: 'Whoops does not match a valid media type!' };
+            break;
         }
     }
-
-    getPopularMovies({ page=1, adult=false} = {}) {
-        return axios
-        .get(popular(this.API_KEY, page))
-        .then(response => {
-            let { page, total_results, total_pages, results: movies } = response.data;
-            movies = adult ? movies : movies.filter(movie => !movie.adult);
-            this.buckets.popular = [...this.buckets.popular, movies];
-            return {
-                get: () => {
-                    return this.buckets.popular;
-                },
-                next: async () => {
-                    const movies = await this.getPopularMovies({page: ++page});
-                    return this.buckets.popular;
-                }
-            };
-        })
-        .catch(error => { 
-            console.log(error);
-            throw Error('Failed to get popular movies'); 
-        });
+    async getPopularMovies(options = { page: 1, adult: false }) {
+        const { page, adult } = options;
+        const apiRoute = this.generateAPI('movie/popular', this.API_KEY, [
+            { key: 'page', value: page }
+        ])
+        const movies = await axios.get(apiRoute).then(response => adult ? response.data.results : response.data.results.filter(movie => !movie.adult));
+        return movies;
     }
 
-    getTopRated({ page=1, adult=false} = {}) {
-        return axios.get(topRated(this.API_KEY, page))
-                    .then( response => {
-                        console.log(response.data);
-                    }) 
+    async getTopRated(options = { page: 1, adult: false }) {
+        const { page, adult } = options;
+        const apiRoute = this.generateAPI('movie/top_rated', this.API_KEY, [
+            { key: 'page', value: page }            
+        ])
+        const movies = await axios.get(apiRoute).then(response => adult ? response.data.results : response.data.results.filter(movie => !movie.adult));
+        return movies;
     }
 
     async init() {
-        return 'test';
+        const apiRoute = this.generateAPI('configuration', this.API_KEY);
+        const { data } = await axios.get(apiRoute);
+        this.config = data;
+        return this;
     }
 }
 
